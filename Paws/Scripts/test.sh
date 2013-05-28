3c050b6bbf92eed4ce668982ef1df694171c5457
elliottcable <me@ell.io>
2013-05-28 06:26:59 -0400

    (- meta) Moving `cake test` into a script ... sort of

---
+++ Paws/Scripts/test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# Usage:
+# ------
+# This simply invokes `mocha` with some sensible defaults.
+# 
+#     FLAGS="--reporter dot --watch" npm run-script test
+#     FLAGS="--grep API" npm run-script test
+# 
+# Unfortunately, at the moment, there's no way to pass flags to `npm run-script`; so I roll in flags
+# using an environment-variable (`$FLAGS`). See: https://github.com/isaacs/npm/issues/3494
+
+env NODE_ENV="$npm_package_config_mocha_ENV"       \
+./node_modules/.bin/mocha                          \
+   --compilers coffee:coffee-script                \
+   --reporter "$npm_package_config_mocha_reporter" \
+   --ui "$npm_package_config_mocha_ui"             \
+   $FLAGS "$npm_package_config_testFiles"
