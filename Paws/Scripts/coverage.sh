69d1242b6c506c2abf2d886b2edbcd57a8ab0715
elliottcable <me@ell.io>
2014-10-02 11:20:22 -0500

    (- new meta debug) Add debugging flags & output to some of the scripts

--- Paws/Scripts/coverage.sh
+++ Paws/Scripts/coverage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-
+                                                                                      set +o verbose
 # Usage:
 # ------
 # This will run 'coffee-coverage' across the codebase, and then generate HTML coverage to file
@@ -7,9 +7,28 @@
 # 
 #     npm run-script coverage
 #     # ./Scripts/coverage.sh 3>./Library/coverage.html
+puts() { printf %s\\n "$*" ;}
 
 if [ "$npm_package_config_mocha_reporter" != 'mocha-lcov-reporter' ]; then
        npm_package_config_mocha_reporter='html-cov'                 ; fi
+
+
+# FIXME: This should really support comma-seperated DEBUG values, as per `node-debug`:
+#        https://github.com/visionmedia/debug
+[ "$DEBUG" = 'Paws.js:scripts' ] && DEBUG_SCRIPTS=0
+[ -n "$DEBUG_SCRIPTS" ] && puts "Script debugging enabled (in: `basename $0`)." >&2
+[ -n "$DEBUG_SCRIPTS" ] && VERBOSE=0
+
+[ -n "$DEBUG_SCRIPTS" ] && puts \
+   "Source dir:            '$npm_package_config_dirs_source'"           \
+   "Instrumentation dir:   '$npm_package_config_dirs_instrumentation'"  \
+   "Test dir:              '$npm_package_config_dirs_test'"             \
+   "Mocha reporter:        '$npm_package_config_mocha_reporter'"        \
+   "Mocha files:           '$npm_package_config_mocha_files'"           \
+   "" >&2
+
+[ -n "$DEBUG_SCRIPTS" ] && set -o verbose
+
 
 ./node_modules/.bin/coffeeCoverage --path=relative \
    --exclude 'additional.coffee' \
--- Paws/Scripts/install-git-hooks.sh
+++ Paws/Scripts/install-git-hooks.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
-
+                                                                                      set +o verbose
 # Usage:
 # ------
 # This script will convert your local .git/hooks directory to allow for multiple scripts for each
 # `git`-hook, and will then install the hooks associated with contributing to this project.
 # 
 #     VERBOSE=true npm run-script install-git-hooks
+puts() { printf %s\\n "$*" ;}
 
 hook_dir=".git/hooks"
 tracked_dir="Scripts/git-hooks"
@@ -14,23 +15,34 @@ requested_hook="$1"
 # ‘Arrays’ in POSIX shell are a nasty topic. It's possible to work relatively sanely with
 # newline-delimited strings, but since git hooks' names contain no spaces, I'll simply use a
 # space-delimited string for this.  /=
-default_hooks="pre-commit post-merge post-checkout"
+default_hooks="$npm_package_config_git_hooks"
 
 if [ ! -d "$hook_dir" ]; then
-   printf %s\\n 'You must be in the root directory of a `git` project to use this script!' >&2
-   exit 1                                                                                       ;fi
+   puts 'You must be in the root directory of a `git` project to use this script!' >&2
+   exit 1                                                                                        ;fi
+
+
+# FIXME: This should really support comma-seperated DEBUG values, as per `node-debug`:
+#        https://github.com/visionmedia/debug
+[ "$DEBUG" = 'Paws.js:scripts' ] && DEBUG_SCRIPTS=0
+[ -n "$DEBUG_SCRIPTS" ] && puts "Script debugging enabled (in: `basename $0`)." >&2
+[ -n "$DEBUG_SCRIPTS" ] && VERBOSE=0
+
+[ -n "$DEBUG_SCRIPTS" ] && puts \
+   "Requested hook:        '$1'"                               \
+   "Default hooks:         '$npm_package_config_git_hooks'"    \
+   "" >&2
 
 
 install_hook() {
    hook_name="$1"
    
-   if [ -n "$VERBOSE" ]; then
-      printf %s\\n 'Installing "'"$hook_name"'" hooks ...' >&2                                  ;fi
+   [ -n "$VERBOSE" ] && puts "Installing '$hook_name' hooks ..." >&2
    
    # If the primary hook-file exists, and isn't a symlink, then we need to move it out of the way.
    if [ ! -h "$hook_dir/$hook_name" -a -x "$hook_dir/$hook_name" ]; then
-      printf %s\\n 'Moving original "'"$hook_name"'" to "'"$hook_name"'-local" ...' >&2
-      mv "$hook_dir/$hook_name" "$hook_dir/$hook_name"'-local' || exit 1                        ;fi
+      puts "Moving original '$hook_name' to '$hook_name-local' ..." >&2
+      mv "$hook_dir/$hook_name" "$hook_dir/$hook_name-local" || exit 1                           ;fi
    
    # If it still exists now, it's either already a symlink or not executable. Either way, don't care
    rm "$hook_dir/$hook_name" 2>/dev/null
@@ -40,15 +52,14 @@ install_hook() {
    
    # For each tracked hook in the repository, 
    for tracked_path in "$tracked_dir/$hook_name"-*; do
-      if [ ! -x "$tracked_path" ]; then continue                                                ;fi
+      if [ ! -x "$tracked_path" ]; then continue                                                 ;fi
       
       hook_path="$hook_dir/$(basename "$tracked_path")"
-      if [ -n "$VERBOSE" ]; then
-         printf %s\\n ' - "'"$hook_path"'" ...' >&2                                             ;fi
+      [ -n "$VERBOSE" ] && puts " - '$hook_path' ..." >&2
       
       # ... remove any existing symlink,
       if [ -L "$hook_path" ]; then
-         rm $hook_path                                                                          ;fi
+         rm $hook_path                                                                           ;fi
       
       # ... and create a symlink to the tracked version of the hook.
       ln -s "../../$tracked_path" "$hook_path" || exit 1
@@ -59,4 +70,4 @@ install_hook() {
 if [ -n "$requested_hook" ]; then 
    install_hook $requested_hook
 else for default_hook in $default_hooks; do
-   install_hook $default_hook          ; done                                                   ;fi
+   install_hook $default_hook          ; done                                                    ;fi
