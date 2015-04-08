dc57db0cfaa1cf068d245f01145ed73b7ef3f2f2
David Balatero <dbalatero@gmail.com>
2015-04-07 22:49:04 -0700

    Only run as root

--- do/you/guys/like/music/listen_to_netcat.sh
+++ do/you/guys/like/music/listen_to_netcat.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # Install and load the netcat kernel module for your listening pleasure.
-#
-#
+
+set -e
+
+if [ "$(id -u)" != "0" ]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
 
 sudo apt-get install build-essential vorbis-tools linux-headers-$(uname -r)
 
