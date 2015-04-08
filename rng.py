40711777de24d30cfe771f172b221cfdf460d8eb
Miles <mileswd@users.noreply.github.com>
2015-04-08 16:54:24 +0100

    Revert "Fix python random number generator."

--- rng.py
+++ rng.py
@@ -1,5 +1,3 @@
-from random import randint
-
 def get_random_number(start=1, end=10):
-    """Generates and returns random number between :start: and :end:"""
-    return randint(start, end)
+    """https://xkcd.com/221/"""
+    return 4
