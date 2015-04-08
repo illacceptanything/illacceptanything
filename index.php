00287b112200ffadadf192f3193731b5a2d6fe7b
one <ekminot@gmail.com>
2015-04-07 21:16:54 -0500

    add a random imgur embed
    Just look at the cute dog

--- index.php
+++ index.php
@@ -20,5 +20,12 @@
 <?php
 echo 'fart'; 
 ?>
+<blockquote class="imgur-embed-pub" lang="en" data-id="kJpHkXO">
+	<a href="//imgur.com/kJpHkXO">
+		8 month old boxer, Leia, sucks on her pacifier in order to fall asleep and currently melts my heart...
+	</a></blockquote>
+	<script async src="//s.imgur.com/min/embed.js" charset="utf-8">
+		<p>Please enable javascript. :3 </p>
+	</script>
 </body>
 </html>
