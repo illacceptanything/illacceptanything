4b2a300f038086924760f646994e9804ef620454
bravenewprince <bravenewprince@gmail.com>
2015-04-07 23:43:59 -0400

    Update backend.go

--- backend.go
+++ backend.go
@@ -4,4 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("Yep, this should serve content")
+	fmt.Println("As long as the content you're hoping to serve is exactly these strings, and the location you're looking to serve them to is exactly standard out.")
 }
