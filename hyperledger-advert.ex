373c4e51da8cf027cbfbffc9cdbb51470b22c7af
Daniel O'Prey <d@oprey.co.uk>
2015-04-07 21:26:45 -0700

    why not

---
+++ hyperledger-advert.ex
@@ -0,0 +1,10 @@
+defmodule Hyperledger.WereHiring do
+  use Hyperledger.Web, :controller
+  use Ecto.Model
+
+  plug :action
+
+  def engineers(erlang, elixir) do
+    email = team@hyperledger.com
+  end
+end
