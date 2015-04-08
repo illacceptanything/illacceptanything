d02c8a705d82630e7036846cbde8917d62056607
RoxasShadow <webmaster@giovannicapuano.net>
2015-04-09 01:25:48 +0200

    笑?

---
+++ 笑笑笑.rb
@@ -0,0 +1,15 @@
+笑 = Class.new do
+  def 笑笑
+    笑笑笑 File.new('/dev/urandom', ?r).read(Random.rand(笑?) * ?笑.bytes.inject(&:+)).split.map { ?笑 }
+  end
+
+  def 笑笑笑(笑笑笑笑)
+    puts 笑笑笑笑.join
+  end
+
+  def 笑?
+    84
+  end
+end
+
+笑.new.笑笑
