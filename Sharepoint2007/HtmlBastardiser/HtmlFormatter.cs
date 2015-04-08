6f0e881105fb0b22b91ec338a264fd868a531f95
appclay <appclay@appclay.com>
2015-04-09 09:35:52 +1000

    Added Sharepoint 2007

---
+++ Sharepoint2007/HtmlBastardiser/HtmlFormatter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FontTagGenerator;
+
+namespace HtmlBastardiser
+{
+    public class HtmlFormatter
+    {
+        public static string Bastardise(string html)
+        {
+            return html
+                .Replace("\r", String.Empty)
+                .Replace("\n", String.Empty)
+                .Replace("\t", String.Empty)
+                .FontTagMe()
+                .Reverse()
+                .ToString();
+        }
+    }
+}
