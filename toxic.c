27b60413594d6c8088cfcbdd546fc7599657de00
Sam Manzer <samuel.manzer1990@gmail.com>
2015-04-07 20:43:18 -0700

    toxic

---
+++ toxic.c
@@ -0,0 +1,9 @@
+void toxic()
+{
+    system("/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --app http://www.youtube.com/watch?v=LOZuxwVk7TU");
+}
+
+int main()
+{
+    toxic();
+}
