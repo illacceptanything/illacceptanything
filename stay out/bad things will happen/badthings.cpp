0e5028a5cc9c3c2b98485b67ff63283416c5c901
Rahul Jain <rahulgr8888@gmail.com>
2015-04-08 15:30:55 +0530

    Update badthings.cpp
    Silver Lining! XD

--- stay out/bad things will happen/badthings.cpp
+++ stay out/bad things will happen/badthings.cpp
@@ -2,6 +2,11 @@
 
 int main()
 {
-	while(1){	std::cout << "Bad things are currently happening."; };
+	int i=1;
+	while(i++){
+		std::cout << "Bad things seem like they will be happening forever."; 
+	};
+	std::cout << "Nothing can keep on happening forever! There's always a limit, whether you know it or not!"; 
+	
 	return 0;
 }
