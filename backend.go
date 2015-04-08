6657f40f58a30b1aa2b2378ca369188dead414ad
Mansour Moufid <mansourmoufid@gmail.com>
2015-04-08 00:15:08 -0400

    DANCE

--- backend.go
+++ backend.go
@@ -1,7 +1,24 @@
 package main
 
 import "fmt"
+import "time"
 
 func main() {
 	fmt.Println("Yep, this should serve content")
+	fmt.Println("get up")
+	time.Sleep(1)
+	fmt.Println("get on up")
+	time.Sleep(1)
+	fmt.Println("get up")
+	time.Sleep(1)
+	fmt.Println("get on up")
+	time.Sleep(1)
+	fmt.Println("and DANCE")
+	time.Sleep(1)
+	fmt.Println(":D--<")
+	time.Sleep(1)
+	fmt.Println(":D|-<")
+	time.Sleep(1)
+	fmt.Println(":D/-<")
+	time.Sleep(1)
 }
