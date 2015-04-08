048d763144653ee12026ee0c5e5265765abcabde
punkcoder <james.mckee.jr@gmail.com>
2015-04-08 09:38:01 -0500

    Application was missing arbitrary business logic.

---
+++ arbitraryBusinessLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace illacceptanything
+{
+    public class arbitraryBusinessLogic
+    {
+        public double GetNumberOne(string user)
+        {
+            if (!string.IsNullOrEmpty(user) && user == "Dave")
+            {
+                Random rand = new Random();
+                double gate = rand.NextDouble();
+
+                if (gate > .3)
+                {
+                    return .9999999993;
+                }
+            }
+            return 1.0;
+        }
+    }
+}
