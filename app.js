83bf3abd47053a72706aaf4a65185dda55d9f7c6
Bucky Schwarz <hoorayimhelping@users.noreply.github.com>
2015-04-08 00:20:39 -0400

    welp, that was fun for 5 minutes.

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 console.log("insert app.js here, be sure to include all the js libraries ever.");
-// TODO: write the rest of app.js
+
+var all_the_js_libraries_ever = []; // TODO: someone fill this in
+
+var App = function() {
+  this.app = 'app';
+  return this;
+};
+
+App.prototype = {
+  init: function() {
+    return this;
+  },
+
+  includeLibraries: function() {
+    for (var i = 0; i < all_the_js_libraries_ever.length; i++) {
+      // the novelty has worn off by this point
+    }
+  }
+}
