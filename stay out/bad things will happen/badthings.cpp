67bdedd0793bb10ebed440e2535c824260f28072
CarpeNecopinum <CarpeNecopinum@users.noreply.github.com>
2015-04-10 14:25:09 +0200

    Need an infinite loop? Why not Zoidberg?

--- stay out/bad things will happen/badthings.cpp
+++ stay out/bad things will happen/badthings.cpp
@@ -2,6 +2,8 @@
 
 int main()
 {
-	while (1) { std::cout << "Bad things are currently happening." << std::endl; }
+	for (;;) { 
+		std::cout << "Bad things are currently happening." << std::endl;
+	}
 	return 0;
 }
