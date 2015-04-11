36d5328cf4ee64df82e6c3a59359b0f9593287b8
My Name <nobody@mail.blox.xyz>
2015-04-11 19:23:51 +0000

    Implementing memewave generator

---
+++ code/ayylmao.c
@@ -0,0 +1,29 @@
+/* gcc -o ayylmao ayylmao.c -lm */
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define AMP 50.
+
+int main( int argc, char *argv[] ){
+	double i;
+	double meh;
+	char *msg = "ayylmao";
+
+	for ( i=0;
+			true;
+			i+=0.1
+			)
+	{
+		meh =(AMP*(sin(i)+2))-(AMP-strlen(msg));printf( "%""3"
+		".""f"" |"""" ""%""""*""s""\n",
+		   meh -
+		strlen( msg ), (
+		unsigned)
+		round( meh), msg );usleep(40000);}
+
+	return
+		0;
+}
