bb4895efca9e847df0a18cb8dc491c4dde4d5276
seyfer <seyferseed@mail.ru>
2015-09-05 18:46:48 +0700

    fart refactoring

--- code/index.php
+++ code/index.php
@@ -3,28 +3,29 @@
 <head lang="en">
     <meta charset="UTF-8">
     <title>Anything you want</title>
-	<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-        <script src="http://code.onion.com/fartscroll.js"></script>
-        <script type="text/javascript">
-                jQuery(document).ready(function(){
-                fartscroll(200);
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <script src="http://code.onion.com/fartscroll.js"></script>
+    <script type="text/javascript">
+        jQuery(document).ready(function () {
+            fartscroll(200);
         });
-        </script>
-	<style type="text/css">
-		body {
-			min-height: 2000px;
-		}
-	</style>
+    </script>
+    <style type="text/css">
+        body {
+            min-height: 2000px;
+        }
+    </style>
 </head>
 <body>
 <?php
-try {
-    echo 'fart'; 
-} catch (PoopException $e) {
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
-}
+
+//TODO:: add some OOP
+
+\Ass\FartGenerator::generate();
+
 ?>
 <!-- best lyrics ever -->
-<iframe width="560" height="315" src="https://www.youtube.com/embed/2HQaBWziYvY?autoplay=1" frameborder="0" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/2HQaBWziYvY?autoplay=1" frameborder="0"
+        allowfullscreen></iframe>
 </body>
 </html>
