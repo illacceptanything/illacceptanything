57dcf12863d990f40bd6269aee67f1afcda8fb16
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2020-04-24 14:54:59 -0400

    Create index.sh

---
+++ riversiderocks/index.sh
@@ -0,0 +1,56 @@
+echo We're no strangers to love
+echo You know the rules and so do I
+echo A full commitment's what I'm thinking of
+echo You wouldn't get this from any other guy
+echo I just wanna tell you how I'm feeling
+echo Gotta make you understand
+echo Never gonna give you up
+echo Never gonna let you down
+echo Never gonna run around and desert you
+echo Never gonna make you cry
+echo Never gonna say goodbye
+echo Never gonna tell a lie and hurt you
+echo We've known each other for so long
+echo Your heart's been aching but you're too shy to say it
+echo Inside we both know what's been going on
+echo We know the game and we're gonna play it
+echo And if you ask me how I'm feeling
+echo Don't tell me you're too blind to see
+echo Never gonna give you up
+echo Never gonna let you down
+echo Never gonna run around and desert you
+echo Never gonna make you cry
+echo Never gonna say goodbye
+echo Never gonna tell a lie and hurt you
+echo Never gonna give you up
+echo Never gonna let you down
+echo Never gonna run around and desert you
+echo Never gonna make you cry
+echo Never gonna say goodbye
+echo Never gonna tell a lie and hurt you
+echo Never gonna give, never gonna give
+echo (Give you up)
+echo (Ooh) Never gonna give, never gonna give
+echo (Give you up)
+echo We've known each other for so long
+echo Your heart's been aching but you're too shy to say it
+echo Inside we both know what's been going on
+echo We know the game and we're gonna play it
+echo I just wanna tell you how I'm feeling
+echo Gotta make you understand
+echo Never gonna give you up
+echo Never gonna let you down
+echo Never gonna run around and desert you
+echo Never gonna make you cry
+echo Never gonna say goodbye
+echo Never gonna tell a lie and hurt you
+echo Never gonna give you up
+echo Never gonna let you down
+echo Never gonna run around and desert you
+echo Never gonna make you cry
+echo Never gonna say goodbye
+echo Never gonna tell a lie and hurt you
+echo Never gonna give you up
+echo Never gonna let you down
+echo Never gonna run around and desert you
+echo Never gonna make you cry
