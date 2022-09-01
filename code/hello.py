fa7a7e74e12002e7282fe5036b89b33339222be0
Andy <57576217+AndyReckt@users.noreply.github.com>
2022-09-01 14:06:20 +0200

    Added hello.py

--- code/hello.py
+++ code/hello.py
@@ -1,1 +1,16 @@
-print("Hey! What's Up")
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi(input("What is your name? "))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
