f69c4b138c6f66ca5c3835539734844fafe9d246
AbdoullahBougataya <88185690+AbdoullahBougataya@users.noreply.github.com>
2023-02-09 14:23:04 +0100

    Virus Free

---
+++ VirusFree.py
@@ -0,0 +1,4 @@
+print("Your Device has been hacked your personnal credentials has been leaked:   ")
+print("email: ilovepotatoes@hacker.рф")
+print("password: 12345abcd")
+print("The reality that you think is real is no more real, because Reality change")
